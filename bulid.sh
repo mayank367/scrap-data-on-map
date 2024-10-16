95063118f985f4c6ab174f77476df4274ba045ae
mayank367 <117195161+mayank367@users.noreply.github.com>
2024-10-16 13:24:59 +0530

    Create bulid.sh

---
+++ bulid.sh
@@ -0,0 +1,12 @@
+you,15 hours ago | 1 author (you)
+#! /usr/bin/env bash
+# exit on error
+set -o errexit
+pip install -r requirements.txt
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+if[[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+fi  
