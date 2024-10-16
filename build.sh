a2f2fafa5a3e3c81590c8cd99f88adc579e67eb5
mayank367 <117195161+mayank367@users.noreply.github.com>
2024-10-16 13:54:14 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
-#! /usr/bin/env bash
-# exit on error
+#!/usr/bin/env bash
+# Exit on error
 set -o errexit
+
+# Install dependencies
 pip install -r requirements.txt
 
+# Collect static files
 python manage.py collectstatic --no-input
+
+# Apply database migrations
 python manage.py migrate
-if[[ $CREATE_SUPERUSER ]];
-then
+
+# Create superuser if the environment variable is set
+if [[ $CREATE_SUPERUSER ]]; then
   python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
-fi  
+fi
